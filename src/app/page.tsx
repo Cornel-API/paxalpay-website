dfbcf816a442a6be33e6e7533d87ffe31e016698
caprule <folabas2@gmail.com>
2025-11-26 13:01:32 +0000

    feat: Implement initial homepage structure with a new Hero component.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
   return (
     <SplashScreen>
       <main>
-        <div className="-mx-[28px] md:mx-0">
+        <div className="-mx-[18px] md:mx-0">
           <Hero />
         </div>
         <LicenseSection />
