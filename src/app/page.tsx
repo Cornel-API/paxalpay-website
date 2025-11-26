209f1929f31e53e8524dc08186c14eb27b8338a2
caprule <folabas2@gmail.com>
2025-11-26 19:03:31 +0000

    feat: add Hero component and establish initial Home page structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
   return (
     <SplashScreen>
       <main className="">
-        <Hero />
+        {/* <Hero /> */}
         <LicenseSection />
         <FeaturesStrip />
         <ThreeTapsSection />
