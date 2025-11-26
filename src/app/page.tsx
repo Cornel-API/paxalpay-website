2fd1e061fe902cd2be2c9b8e2355c33f332653be
caprule <folabas2@gmail.com>
2025-11-26 13:23:38 +0000

    px15

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,7 @@ import SplashScreen from "@/components/SplashScreen";
 export default function Home() {
   return (
     <SplashScreen>
-      <main className="-mr-[20px]">
+      <main className="-mr-[15px]">
         <div className=" md:mx-0">
           <Hero />
         </div>
