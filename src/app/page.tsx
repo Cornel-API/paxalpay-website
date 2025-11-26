b37171279108db82e03337f82d2321b16a19e363
caprule <folabas2@gmail.com>
2025-11-26 20:02:06 +0000

    exchang

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,7 +24,7 @@ export default function Home() {
         {/* <About /> */}
 
         {/* <AutoTabs /> */}
-        <ExchangeRatesSection />
+        {/* <ExchangeRatesSection /> */}
         {/* <SupportSection /> */}
         <PeopleSection />
         <FAQSection />
